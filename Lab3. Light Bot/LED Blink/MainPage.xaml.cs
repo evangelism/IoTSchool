fc20e34d56371adff02ab7224408e46119efb65e
Dmitri Soshnikov <dmitri@soshnikov.com>
2016-10-28 10:45:49 +0300

    Add initial work on Lab3, Update Readme

---
+++ Lab3. Light Bot/LED Blink/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Azure.Devices.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LED_Blink
+{
+
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        const string HubConnectionString = "HostName=samplehub13.azure-devices.net;DeviceId=RPi;SharedAccessKey=ZWnxUTH194FsiQrMrfsC8z5xW/oS3TfkAaVcIXLgZ1Q=";
+
+        public DeviceClient hub;
+
+        GpioPin pin = null;
+        bool state = false;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var gpio = GpioController.GetDefault();
+
+            // Если приложение будет запущено на компьютере без IoT,
+            // то gpio==null. Поэтому добавляем проверки на null,
+            // чтобы приложение работало и на desktop/mobile
+            pin = gpio?.OpenPin(17);
+            pin?.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            hub = DeviceClient.CreateFromConnectionString(HubConnectionString);
+            await hub.OpenAsync();
+
+            // Начинаем получать сообщения из IoT Hub
+            // Здесь специально нет await, т.к. нам не надо ожидать окончания выполнения
+            Receive();
+        }
+
+        private async void Blink(object sender, object e)
+        {
+            state = !state;
+            LED(state);
+            await Send(state ? "on" : "off");
+        }
+
+        private async Task Send(string s)
+        {
+            await hub.SendEventAsync(new Message(Encoding.UTF8.GetBytes(s)));
+        }
+
+        private void LED(bool state)
+        {
+            vled.Fill = new SolidColorBrush(state ? Colors.Red : Colors.LightGray);
+            if (pin != null)
+            {
+                pin.Write(state ? GpioPinValue.High : GpioPinValue.Low);
+            }
+        }
+
+        private async Task Receive()
+        {
+            await Task.Delay(1000);
+            while (true)
+            {
+                var msg = await hub.ReceiveAsync();
+                if (msg != null)
+                {
+                    var s = Encoding.ASCII.GetString(msg.GetBytes());
+                    LED(s=="on");
+                    await hub.CompleteAsync(msg);
+                }
+            }
+        }
+    }
+}
---
+++ Lab3. Light Bot/LED Bot/Controllers/MessagesController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices;
+using System.Text;
+
+namespace LED_Bot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                var txt = activity.Text.ToLower();
+                var res = "Oops!";
+                if (txt.StartsWith("hostname="))
+                {
+                    Store.RegisterClient(activity.From.Id, txt);
+                    res = "Hub registered!";
+                }
+                else if (txt == "on")
+                {
+                    res = await Process(activity.From.Id, true);
+                }
+                else if (txt == "off")
+                {
+                    res = await Process(activity.From.Id, false);
+                }
+                else res = "I do not understand you.";
+
+                Activity reply = activity.CreateReply(res);
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private async Task<string> Process(string id, bool v)
+        {
+            var hcs = Store.GetConnString(id);
+            if (hcs == null) return "Hub not configured, please send me connection string";
+            var hub = ServiceClient.CreateFromConnectionString(hcs);
+            var state = v ? "on" : "off";
+            await hub.SendAsync("RPi", new Message(Encoding.UTF8.GetBytes(state)));
+            return state;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
